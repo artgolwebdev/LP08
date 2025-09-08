8c8903f98b269e10a30e86fe03fb3bb8e23a0e4c
artgolwebdev <artyombb@gmail.com>
2025-09-08 14:01:24 +0300

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/LP08/',
+  base: process.env.NODE_ENV === 'production' ? '/LP08/' : '/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
